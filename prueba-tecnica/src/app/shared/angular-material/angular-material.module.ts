e0ad415e45c898d3ed2e445d858d27523c91fd21
Daniel151505 <daniel150520011@gmail.com>
2022-09-09 00:17:03 -0500

    Deploy

--- prueba-tecnica/src/app/shared/angular-material/angular-material.module.ts
+++ prueba-tecnica/src/app/shared/angular-material/angular-material.module.ts
@@ -3,67 +3,37 @@ import { CommonModule } from '@angular/common';
 
 // Angular Material
 
-import { MatSliderModule } from '@angular/material/slider';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatTableModule} from '@angular/material/table';
-import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatSortModule} from '@angular/material/sort';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDialogModule} from '@angular/material/dialog';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule} from '@angular/material/core';
 import {MatSelectModule} from '@angular/material/select';
-import {MatRadioModule} from '@angular/material/radio';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
-import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatSliderModule,
-    MatToolbarModule,
-    MatTableModule,
-    MatFormFieldModule,
     MatInputModule,
     MatPaginatorModule,
-    MatSortModule,
     MatIconModule,
     MatDialogModule,
-    MatSnackBarModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
     MatSelectModule,
-    MatRadioModule,
     MatButtonModule,
     MatCardModule,
-    MatTooltipModule,
     MatDividerModule,
     MatProgressSpinnerModule
   ],
   exports: [
-    MatSliderModule,
-    MatToolbarModule,
-    MatTableModule,
-    MatFormFieldModule,
     MatInputModule,
     MatPaginatorModule,
-    MatSortModule,
     MatIconModule,
     MatDialogModule,
-    MatSnackBarModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
     MatSelectModule,
-    MatRadioModule,
     MatButtonModule,
     MatCardModule,
-    MatTooltipModule,
     MatDividerModule,
     MatProgressSpinnerModule
   ]
